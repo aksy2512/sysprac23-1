e934d053059e544ac1a1f8f86660799c5c28cd01
ashu3103 <ash2003sharma@gmail.com>
2023-04-20 15:26:42 +0530

    excel to csv

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # importing all modules
 from DOCX_to_PDF import *
+from Excel_to_CSV import *
 
 if __name__ == '__main__':
     # important to avoid recursion
     # test dox to pdf
-    dir = "Data"
-    temp = DOCX2PDF(dir)
-
+    # dir = "Data"
+    # temp = DOCX2PDF(dir)
+    #test excel to csv
+    dir = "ExcelData"
+    temp = EXCEL2CSV(dir)
