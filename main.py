be3de4e1285386ec22abfa653b4ebb55c931cf78
aksy2512 <b20147@students.iitmandi.ac.in>
2023-04-20 17:23:51 +0530

    Added some changes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # importing all modules
 from DOCX_to_PDF import *
-
+from PDF_to_DOCX import *
 if __name__ == '__main__':
     # important to avoid recursion
     # test dox to pdf
     dir = "Data"
-    temp = DOCX2PDF(dir)
+    # temp = DOCX2PDF(dir)
+    temp = PDF2DOCX(dir)
+
 
